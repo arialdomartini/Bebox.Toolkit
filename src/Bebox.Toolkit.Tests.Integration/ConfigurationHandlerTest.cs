23a6d65443a53b9de8107bb98f0a54ef9bcf26e9
Arialdo Martini <arialdomartini@gmail.com>
2012-08-06 16:01:00 +0200

    ConfigurationHandler is able to deserialize an object from App.config

---
+++ src/Bebox.Toolkit.Tests.Integration/ConfigurationHandlerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+using System.Linq;
+
+namespace Bebox.Toolkit.Tests.Integration
+{
+	[TestFixture]
+	public class ConfigurationHandlerTest
+	{
+		[Test]
+		public void Should_be_able_to_deserialize_a_sample_class()
+		{
+			var sampleParentClass = System.Configuration.ConfigurationSettings.GetConfig("samplesection") as SampleParentClass;
+
+			sampleParentClass.Name.Should().Be.EqualTo("John");
+			sampleParentClass.Surname.Should().Be.EqualTo("Lumbs");
+
+			sampleParentClass.Children.Count.Should().Be.EqualTo(2);
+			sampleParentClass.Children.SingleOrDefault(c => c.Value == 1).Should().Not.Be.Null();
+			sampleParentClass.Children.SingleOrDefault(c => c.Value == 2).Should().Not.Be.Null();
+		}
+	}
+
+	public class SampleConfigurationHandler : ConfigurationHandler<SampleParentClass> { }
+
+	public class SampleParentClass
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public List<SampleChild> Children { get; set; }
+	}
+
+	public class SampleChild
+	{
+		public int Value { get; set; }
+	}
+}
---
+++ src/Bebox.Toolkit/ConfigurationHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Bebox.Toolkit
+{
+	public class ConfigurationHandler<T> : IConfigurationSectionHandler
+	{
+		public object Create(object parent, object configContext, XmlNode section)
+		{
+			var environmentNode = section.SelectSingleNode(typeof(T).Name);
+			var xmlSerializer = new XmlSerializer(typeof(T));
+			var xmlNodeReader = new XmlNodeReader(environmentNode);
+			return xmlSerializer.Deserialize(xmlNodeReader);
+		}
+	}
+}
